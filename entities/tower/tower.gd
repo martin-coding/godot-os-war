7d5e84a5e06728523b04985d62410f04d250bd35
martin-coding <martinkoehn.coding@gmail.com>
2023-11-30 14:05:46 +0100

    Changed how shooting cooldown works
    Now the cooldown only starts after a successful fire and the progress bar will disappear when tower is ready

--- entities/tower/tower.gd
+++ entities/tower/tower.gd
@@ -3,6 +3,7 @@ extends Node2D
 var bullet = preload("res://entities/tower/bullet.tscn")
 var enemies = []
 var current_enemy
+var ready_to_fire = false
 
 @onready var sprite_reference = get_node("Sprite2D")
 
@@ -17,11 +18,21 @@ func _physics_process(_delta):
 	if enemies != []:
 		current_enemy = enemies[0]
 		sprite_reference.rotation = global_position.angle_to_point(current_enemy.global_position) - 3.1 # the "- 3.1 fixes a weird rotation" | uncomment line above if u want funny tux
+		if ready_to_fire == true:
+			ready_to_fire = false
+			var b = bullet.instantiate()
+			shoot_timer.start()
+			b.global_position = global_position
+			b.target = current_enemy
+			get_parent().add_child(b)
 
 
 func _process(delta):
-	shoot_progress_bar.visible = is_instance_valid(current_enemy)
 	shoot_progress_bar.value = shoot_timer.time_left
+	if shoot_progress_bar.value == 0:
+		shoot_progress_bar.visible = false
+	else:
+		shoot_progress_bar.visible = true
 	
 func _on_sight_area_area_entered(area):
 	if area.is_in_group("enemy"):
@@ -34,10 +45,4 @@ func _on_sight_area_area_exited(area):
 
 
 func _on_shoot_timer_timeout():
-	if current_enemy:
-		if enemies:
-			if current_enemy == enemies[0]:
-				var b = bullet.instantiate()
-				b.global_position = global_position
-				b.target = current_enemy
-				get_parent().add_child(b)
+	ready_to_fire = true
