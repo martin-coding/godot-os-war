6110ae18e385cc648f6b2480400b682dfb53cd36
EinsKatze <82060534+EinsKatze@users.noreply.github.com>
2023-12-02 12:40:35 +0100

    Fix enemies spawning on top of each other

--- main.gd
+++ main.gd
@@ -1,28 +1,28 @@
 extends Node2D
 
-var spawners = []
+
 var health: int = 100
 
 @onready var enemy = preload("res://entities/enemy.tscn")
 @onready var healthLbl = get_node("UI Control/Health")
+@onready var spawners: Array = get_tree().get_nodes_in_group("spawn_location")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spawners = get_tree().get_nodes_in_group("spawn_location")
-	
 	eventmanager.connect("on_player_take_damage", _on_damage_player)
-	
+
 	for spawner in spawners:
 		var new_enemy = enemy.instantiate()
 		new_enemy.speed = randf_range(60.0, 120.0)
+		new_enemy.set_progress(randf_range(0.0, 20.0)) # avoid enemies spawning exactly on top of each other
 		spawner.add_child(new_enemy)
 
 
 func _on_timer_timeout():
-	spawners = get_tree().get_nodes_in_group("spawn_location")
 	for spawner in spawners:
 		var new_enemy = enemy.instantiate()
 		new_enemy.speed = randf_range(40.0, 120.0)
+		new_enemy.set_progress(randf_range(0.0, 20.0))
 		spawner.add_child(new_enemy)
 
 
