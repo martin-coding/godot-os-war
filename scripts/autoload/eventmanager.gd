88c656f1e1c4aa30b77cfd98f2358e34424ed2ba
Mathew <mathew.tran.dev@gmail.com>
2023-12-04 08:13:39 -0800

    starting work on tower placement

--- scripts/autoload/eventmanager.gd
+++ scripts/autoload/eventmanager.gd
@@ -1,6 +1,10 @@
 extends Node
 
 signal on_player_take_damage(amount : float)
+signal tower_button_pressed(towerClass)
 
 func broadcast_player_take_damage(amount):
 	emit_signal("on_player_take_damage", amount)
+
+func broadcast_tower_button_pressed(towerClass):
+	emit_signal("tower_button_pressed", towerClass)
